de45941e3a66434a29028489187df4146fef620a
Sergio Monge <smonge630@gmail.com>
2026-06-11 15:28:54 -0500

    lazygit mappings added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,6 +3,7 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local which_key = require "which-key"
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -13,3 +14,19 @@ map("n", "<C-t>", function()
   require("nvchad.themes").open { style = "flat" }
 end, {})
 map("n", "<S-t>", "<cmd>FloatermToggle<cr>", {})
+
+which_key.add {
+  { "<leader>g", desc = "Lazygit" },
+}
+
+map("n", "<leader>gg", function()
+  ---@param opts? snacks.lazygit.Config
+  Snacks.lazygit.open(opts)
+end, { desc = "Open Lazygit" })
+map("n", "<leader>gl", function()
+  Snacks.lazygit.log()
+end, { desc = "Lazygit log" })
+map("n", "<leader>gf", function()
+  ---@param opts? snacks.lazygit.Config|{}
+  Snacks.lazygit.log_file(opts)
+end, { desc = "Lazygit log file" })
