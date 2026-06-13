d4f640749830a4e489ae9f5a8f1aba4a6d52cefd
Sergio Monge <smonge630@gmail.com>
2026-06-13 00:10:12 -0500

    showkeys plugin added and mappings

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "neofusion",
+  theme = "alien_blood",
 
   hl_override = {
     Comment = { italic = true },
---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,11 @@
+return {
+  "nvzone/showkeys",
+  cmd = "ShowkeysToggle",
+  lazy = false,
+  opts = {
+    timeout = 1,
+    maxkeys = 5,
+    position = "bottom-center",
+    -- more opts
+  },
+}
